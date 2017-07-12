8af8ba734578825f032dfa7cce282cb4a2ffc2b3
diogomarvao <diogomarvao@hotmail.com>
2017-07-12 10:05:52 +0100

    repository

---
+++ Event/src/main/java/com/musicstadium/service/EntityService.java
@@ -0,0 +1,21 @@
+package com.musicstadium.service;
+
+
+import com.musicstadium.model.Entity;
+import com.musicstadium.repository.EntityRepository;
+
+public class EntityService <E extends Entity> {
+	
+	public void addEntity(EntityRepository<E> entityList, E entity){
+		entityList.addToDb(entity);
+	}
+	
+	public void delEntity(EntityRepository<E> entityList, E entity){
+		entityList.removeFromDb(entity);
+	}
+	
+	public void editEntity(EntityRepository<E> entityList, E entity){
+		entityList.alterInDb(entity);
+	}
+	
+}
