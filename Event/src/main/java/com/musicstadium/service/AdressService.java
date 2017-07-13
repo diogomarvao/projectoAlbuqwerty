5e31baf9680f9451899ca6b6e4df80507bad6b6b
luisparreira <luisparreira1988@gmail.com>
2017-07-13 09:34:07 +0100

    2

--- Event/src/main/java/com/musicstadium/service/AdressService.java
+++ Event/src/main/java/com/musicstadium/service/AdressService.java
@@ -1,5 +1,30 @@
 package com.musicstadium.service;
 
-public class AdressService {
+import java.util.List;
+
+import javax.enterprise.context.RequestScoped;
+import javax.inject.Named;
+import javax.persistence.Query;
+import javax.transaction.Transactional;
+
+import com.musicstadium.model.Adress;
+import com.musicstadium.repository.EntityRepository;
+
+@Named("adressService")
+@RequestScoped
+public class AdressService extends EntityRepository<Adress>{
+	
+	public List<Adress> getDbAdress() {
+		Query query = getDb().createQuery("FROM Adress");
+		List<Adress> dbElements = (List<Adress>) query.getResultList();
+		return dbElements;
+	}
+	
+	@Override
+	@Transactional
+	public void removeFromDb(Adress adress){
+		Adress activeAdress = getDb().find(Adress.class, adress.getId());
+		getDb().remove(activeAdress);
+	}
 
 }
